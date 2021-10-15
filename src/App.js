20802198b119ddb2942fa0f2136d7c71a6218b72
alamin500 <nalamin500@gmail.com>
2021-10-15 08:46:34 +0600

    h

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import './App.css';
-import Home from './Pages/Home/Home/Home';
-
+import "./App.css";
+import Home from "./Pages/Home/Home/Home";
+import NotFound from "./Pages/NotFound/NotFound";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Booking from "./Pages/Booking/Booking";
+import Login from "./Pages/Login/Login/Login";
+import Header from "./Pages/Shared/Header/Header";
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
+      <Router>
+        <Header></Header>
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+          <Route path="/booking/:serviceId">
+            <Booking></Booking>
+          </Route>
+          <Route path="/login">
+            <Login></Login>
+          </Route>
+          <Route path="*">
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
